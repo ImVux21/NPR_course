d93f14b7f1b691d9dbf72e31b62a9b8dd7514365
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2022-12-26 11:34:22 +0700

    NPR-UDP socket with GUI

---
+++ src/udp/client/Client.java
@@ -0,0 +1,78 @@
+package udp.client;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import javax.swing.*;
+
+public class Client extends JFrame {
+
+    private final JTextField enterField;
+    private final JTextArea displayArea;
+
+    // create datagram socket
+    private DatagramSocket socket;
+
+    public Client() {
+        super("Client");
+
+        enterField = new JTextField("Enter text here");
+        enterField.addActionListener(event -> {
+            try {
+                // create and send packet
+                byte[] data = event.getActionCommand().getBytes();
+                DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 5000);
+                socket.send(packet);
+
+                // display packet contents
+                display("\nSent: " + event.getActionCommand());
+                enterField.setText("");
+            } catch (IOException e) {
+                display("\nError sending packet");
+            }
+        });
+
+        add(enterField, "North");
+
+        displayArea = new JTextArea();
+        add(new JScrollPane(displayArea), "Center");
+
+        setSize(400, 300);
+        setVisible(true);
+
+        try {
+            socket = new DatagramSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void waitForPackets() {
+        while (true) {
+            try {
+                // receive packet
+                byte[] data = new byte[100];
+                DatagramPacket packet = new DatagramPacket(data, data.length);
+                socket.receive(packet);
+
+                // display packet contents
+                display("\nReceived: " + new String(packet.getData()));
+            } catch (IOException e) {
+                display("\nError receiving packet");
+            }
+        }
+    }
+
+    private void display(final String messageToDisplay) {
+        SwingUtilities.invokeLater(() -> displayArea.append(messageToDisplay));
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.waitForPackets();
+    }
+}
+
---
+++ src/udp/server/UDPServer.java
@@ -0,0 +1,79 @@
+package udp.server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import javax.swing.*;
+
+public class UDPServer extends JFrame {
+
+    private final JTextField enterField;
+    private final JTextArea displayArea;
+
+    // create datagram socket and bind to port
+    private DatagramSocket socket;
+
+    public UDPServer() {
+        super("Server");
+
+        enterField = new JTextField("Enter text here");
+        enterField.addActionListener(event -> {
+            try {
+                // create packet
+                byte[] data = event.getActionCommand().getBytes();
+                DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 5000);
+
+                // send packet
+                socket.send(packet);
+
+                // display packet contents
+                display("\nSent: " + event.getActionCommand());
+                enterField.setText("");
+            } catch (IOException e) {
+                display("\nError sending packet");
+            }
+        });
+
+        add(enterField, "North");
+
+        displayArea = new JTextArea();
+        add(new JScrollPane(displayArea), "Center");
+
+        setSize(400, 300);
+        setVisible(true);
+
+        try {
+            socket = new DatagramSocket(5000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void waitForPackets() {
+        while (true) {
+            try {
+                // receive packet
+                byte[] data = new byte[100];
+                DatagramPacket packet = new DatagramPacket(data, data.length);
+                socket.receive(packet);
+
+                // display packet contents
+                display("\nReceived: " + new String(packet.getData()));
+            } catch (IOException e) {
+                display("\nError receiving packet");
+            }
+        }
+    }
+
+    private void display(final String messageToDisplay) {
+        SwingUtilities.invokeLater(() -> displayArea.append(messageToDisplay));
+    }
+
+    public static void main(String[] args) {
+        UDPServer server = new UDPServer();
+        server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        server.waitForPackets();
+    }
+}
